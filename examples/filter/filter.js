672a3d83e8fb7b595c3109525c6c295665bc835c
Chandra Sekar S <chandru.in@gmail.com>
2010-07-06 20:08:40 +0530

    Added filter example.

---
+++ examples/filter/filter.js
@@ -0,0 +1,51 @@
+require.paths.unshift('./lib');
+
+var mvc = require('mvc');
+var sys = require('sys');
+
+mvc.addFilters(/\/secure/, function(nextFilter) {
+    var self = this;
+    this.getSessionValue('user', function(err, user) {
+        if(!err && user) {
+            nextFilter();
+        } else {
+            self.renderError(403);
+        }
+    });
+});
+
+mvc.get('/secure_welcome', function() {
+    this.disableCache();
+    var self = this;
+    this.getSessionValue('user', function(err, user) {
+        self.render('welcome', {user: user});
+    });
+});
+
+mvc.get('/', function() {
+    this.disableCache();
+    var self = this;
+    this.getSessionValue('user', function(err, user) {
+        if(!err && user) {
+            self.redirect('/secure_welcome'); 
+        } else {
+            self.render('login');
+        }
+    });
+});
+
+mvc.post('/login', function() {
+    var self = this;
+    this.setSessionValue('user', this.params['name'], function() {
+        self.redirect('/');
+    });
+});
+
+mvc.get('/logout', function() {
+    var self = this;
+    this.endSession(function() {
+        self.redirect('/');
+    });
+});
+
+mvc.serve(8080);
