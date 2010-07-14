de442837e0169885cf90dfdacd301a0eb0b9e4cd
Chandra Sekar S <chandru.in@gmail.com>
2010-07-14 18:31:48 +0530

    Added file upload support.

---
+++ examples/upload/upload.js
@@ -0,0 +1,20 @@
+require.paths.unshift('./lib');
+
+var gh = require('grasshopper'),
+    fs = require('fs');
+
+gh.get('/', function() {
+    this.render('index');
+});
+
+gh.post('/upload', function() {
+    var upload = this.params['file'];
+    this.model.name = this.params['name'];
+
+    var self = this;
+    fs.rename(upload.path, './uploads/' + upload.filename, function() {
+        self.render('upload');
+    });
+});
+
+gh.serve(8080);
