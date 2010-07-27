573d45890f091405ab3286b69468d2f8c8191f75
Chandra Sekar S <chandru.in@gmail.com>
2010-07-27 17:23:54 +0530

    i18n support.

---
+++ examples/i18n/locales.js
@@ -0,0 +1,21 @@
+var gh = require('./grasshopper');
+
+var locales = {};
+locales['en-us'] = {
+    title: 'Color Page',
+    msg: 'Color'
+};
+locales['en-gb'] = {
+    title: 'Colour Page',
+    msg: 'Colour'
+};
+
+gh.configure({
+    locales: locales
+});
+
+gh.get('/', function() {
+    this.render('index');
+});
+
+gh.serve(8080);
